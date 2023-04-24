ab2bc740f9d4eae02acbf159f5e9eaf61070c3d5
Alexander Kukoba <luckychel@gmail.com>
2023-04-24 22:45:08 +0300

    initial commit

---
+++ VKSwiftUI/VKSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKSwiftUIApp.swift
+//  VKSwiftUI
+//
+//  Created by Александр Кукоба on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VKSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
